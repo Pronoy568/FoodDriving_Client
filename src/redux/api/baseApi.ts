6648c50c10bf230b09170a41342847faf54711b5
Pronoy568 <pronoydas463@gamil.com>
2024-03-20 22:17:57 +0600

    Readme file update

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/",
+    baseUrl: "https://food-driving-server.vercel.app/",
   }),
   tagTypes: ["supplies", "gratitude", "testimonial", "volunteer"],
   endpoints: () => ({}),
